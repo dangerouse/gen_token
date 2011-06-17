a959cc284689b57c2ea435ca1d260c99baa6e4cd
Graeme Rouse <graeme@arizonabay.com>
2011-06-17 11:05:38 -0700

    implemented gen_token. Updated gemspec

--- gen_token.gemspec
+++ gen_token.gemspec
@@ -5,11 +5,11 @@ Gem::Specification.new do |s|
   s.name        = "gen_token"
   s.version     = GenToken::VERSION
   s.platform    = Gem::Platform::RUBY
-  s.authors     = []
-  s.email       = []
-  s.homepage    = "http://rubygems.org/gems/gen_token"
-  s.summary     = "TODO: Write a gem summary"
-  s.description = "TODO: Write a gem description"
+  s.authors     = ["Graeme Rouse"]
+  s.email       = ["graeme@arizonabay.com"]
+  s.homepage    = "http://www.arizonabay.com"
+  s.summary     = "Simply generates a unique token in a database."
+  s.description = "Point GenToken at a database column and it will generate a random and unique key for the field. "
 
   s.required_rubygems_version = ">= 1.3.6"
   s.rubyforge_project         = "gen_token"
--- lib/gen_token.rb
+++ lib/gen_token.rb
@@ -1,3 +1,37 @@
 module GenToken
-  # Your code goes here...
+
+  # Usage:
+  # include GenToken
+  # gen_token :key, :type => :hex, :length => AppConfig.token.token_length
+  # gen_token :password, :type => :hex, :length => AppConfig.token.token_length
+  
+  module GenTokenClassMethods
+    def gen_token(key, options = {})
+      token_def = {:name => key, :type => :hex, :length => 16}.merge(options)
+      write_inheritable_array(:token_fields, [token_def])
+    end
+  end
+  
+  module ActiveRecordInstanceMethods
+    def generate_tokens
+      self.class.read_inheritable_attribute(:token_fields).each do |token|
+        name, type, length = token[:name], token[:type], token[:length]
+        self[token[:name]] = ActiveSupport::SecureRandom.send(:"#{token[:type]}", token[:length])
+        while !self.class.send(:"find_by_#{token[:name]}", self[token[:name]]).nil?
+          self[token[:name]] = ActiveSupport::SecureRandom.send(:"#{token[:type]}", token[:length])
+        end
+      end
+    end
+  end
+  
+  def self.included(klass)
+    klass.extend GenTokenClassMethods
+    if Object.const_defined?(:ActiveRecord)
+      if klass < ActiveRecord::Base
+        klass.send :include, ActiveRecordInstanceMethods
+        klass.before_validation(:generate_tokens, :on => :create)
+      end
+    end
+  end
+  
 end
